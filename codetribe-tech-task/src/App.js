4b638717ec02178328f4503f0e3cd48925b9aca7
Lacedaimon95 <svetozar95@gmail.com>
2022-07-16 21:07:18 +0200

    master/ random colors

--- codetribe-tech-task/src/App.js
+++ codetribe-tech-task/src/App.js
@@ -1,6 +1,30 @@
 import './App.scss';
+import React, { useState } from 'react';
+
 
 function App() {
+  const[colors, setColors] = useState([]);
+  const[level, setLevel] = useState(3)
+
+
+  // random draw
+  const handleDraw = e =>{
+    e.preventDefault();
+    let br = 0;
+    
+    let draw = setInterval(()=>{
+      let random = Math.floor(Math.random()*4)+1;
+      setColors(colors=>[...colors,random]);
+      br++;
+
+      if(br === level){
+        clearInterval(draw)
+      }
+    },1000)
+  }
+
+
+console.log(colors);
   return (
     
     <section className="app">
@@ -10,7 +34,7 @@ function App() {
         <article className="app__box__field3"></article>
         <article className="app__box__field4"></article>
       </div>
-      <button>start</button>
+      <button onClick={(e)=>handleDraw(e)}>start</button>
     </section>
 
   );
