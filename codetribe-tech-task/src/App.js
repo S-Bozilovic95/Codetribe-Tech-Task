cad756ed2b73e3e75250d5a5156fef72b8438dd8
Lacedaimon95 <svetozar95@gmail.com>
2022-07-16 21:15:03 +0200

    master/ step3

--- codetribe-tech-task/src/App.js
+++ codetribe-tech-task/src/App.js
@@ -4,7 +4,8 @@ import React, { useState } from 'react';
 
 function App() {
   const[colors, setColors] = useState([]);
-  const[level, setLevel] = useState(3)
+  const[level, setLevel] = useState(3);
+  const[light, setLight] = useState(0);
 
 
   // random draw
@@ -14,6 +15,7 @@ function App() {
     
     let draw = setInterval(()=>{
       let random = Math.floor(Math.random()*4)+1;
+      setLight(random)
       setColors(colors=>[...colors,random]);
       br++;
 
@@ -29,10 +31,10 @@ console.log(colors);
     
     <section className="app">
       <div className="app__box">
-        <article className="app__box__field1"></article>
-        <article className="app__box__field2"></article>
-        <article className="app__box__field3"></article>
-        <article className="app__box__field4"></article>
+        <article className="app__box__field1" style={light===1?{opacity:"0.5"}:null}></article>
+        <article className="app__box__field2" style={light===2?{opacity:"0.5"}:null}></article>
+        <article className="app__box__field3" style={light===3?{opacity:"0.5"}:null}></article>
+        <article className="app__box__field4" style={light===4?{opacity:"0.5"}:null}></article>
       </div>
       <button onClick={(e)=>handleDraw(e)}>start</button>
     </section>
