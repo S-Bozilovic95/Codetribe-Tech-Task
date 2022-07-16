b84cc60d6e676a03943bcc4c6875b5a0039dacd6
Lacedaimon95 <svetozar95@gmail.com>
2022-07-16 22:37:03 +0200

    master/ style

--- codetribe-tech-task/src/App.js
+++ codetribe-tech-task/src/App.js
@@ -59,14 +59,14 @@ function App() {
 
   return (
     <section className="app">
-      <h2>Round: {level-2}</h2>
+      <h2 className='app__header'>Round: {level-2}</h2>
       <div className="app__box">
         <article className="app__box__field1" style={light===1?{opacity:"0.5"}:null} id={1} onClick={(e)=>handleUserDraw(e,Number(e.target.id))}></article>
         <article className="app__box__field2" style={light===2?{opacity:"0.5"}:null} id={2} onClick={(e)=>handleUserDraw(e,Number(e.target.id))}></article>
         <article className="app__box__field3" style={light===3?{opacity:"0.5"}:null} id={3} onClick={(e)=>handleUserDraw(e,Number(e.target.id))}></article>
         <article className="app__box__field4" style={light===4?{opacity:"0.5"}:null} id={4} onClick={(e)=>handleUserDraw(e,Number(e.target.id))}></article>
       </div>
-      <button onClick={(e)=>handleDraw(e)}>start</button>
+      <button className='app__start' onClick={(e)=>handleDraw(e)}>start</button>
     </section>
   );
 }
