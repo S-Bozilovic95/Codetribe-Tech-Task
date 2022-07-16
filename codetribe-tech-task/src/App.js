69bbf9994c1eab3f7ffa55d7aa7568c5fad9d0b1
Lacedaimon95 <svetozar95@gmail.com>
2022-07-16 20:12:50 +0200

    master/ step1,setep2

--- codetribe-tech-task/src/App.js
+++ codetribe-tech-task/src/App.js
@@ -2,9 +2,17 @@ import './App.scss';
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    
+    <section className="app">
+      <div className="app__box">
+        <article className="app__box__field1"></article>
+        <article className="app__box__field2"></article>
+        <article className="app__box__field3"></article>
+        <article className="app__box__field4"></article>
+      </div>
+      <button>start</button>
+    </section>
+
   );
 }
 
